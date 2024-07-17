882626c845f40d4c467a7124275a36afdbe807b5
Leena <leenacruz@gmail.com>
2024-07-16 20:01:12 -0700

    Api/notes GET and POST request works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,31 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const router = express.Router();
 // const notesData = require('./public/db/db.json');
 
-const PORT = 5500;
+
 const fs = require('fs');
-const notesData = JSON.parse(fs.readFileSync('./db/db.json'));
+
 // get route for notes
 // check readme
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
   //should read the `db.json` file and return all saved notes as JSON.
   // console.log("funciona")
   // return res.json(notesData);
   // res.json(path.join(__dirname, '../db/db.json'));
+  const notesData = JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(notesData);
 });
 
 //* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 
-router.post('/api/notes', (req,res) => {
-  fs.writeFile('db.json', res)
+router.post('/notes', (req,res) => {
+  res.send('Its working!');
+  console.log(req.body);
+  fs.writeFile('db.json', req.body)
 })
-
-
-
 
 
   module.exports = router;
