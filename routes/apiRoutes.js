1c4ba5e060313b1d27f332dd7f3f5d23ca15d653
Leena <leenacruz@gmail.com>
2024-07-16 20:51:07 -0700

    Fixes in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,16 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-// const notesData = require('./public/db/db.json');
-
-
 const fs = require('fs');
 
-// get route for notes
-// check readme
-
 router.get('/notes', (req, res) => {
-  //should read the `db.json` file and return all saved notes as JSON.
-  // console.log("funciona")
-  // return res.json(notesData);
-  // res.json(path.join(__dirname, '../db/db.json'));
   const notesData = JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(notesData);
 });
 
-//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
-
 router.post('/notes', (req,res) => {
-  res.send('Its working!');
-  console.log(req.body);
-  fs.writeFile('db.json', req.body)
+//Writes/Append the note data into the db.json file.
+  fs.appendFileSync('./db/db.json', JSON.stringify(req.body))
+ 
 })
 
 
